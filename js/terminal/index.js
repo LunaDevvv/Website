a7755fee1b4fd7b11cb0f1681df634f240aeffb9
ShiroDevv <ayviej07@gmail.com>
2023-01-25 13:16:02 -0500

    Basic terminal functionality.

--- js/terminal/index.js
+++ js/terminal/index.js
@@ -1,7 +1,7 @@
 const terminalDiv = document.getElementById("terminal");
 
 let textArray = `
-Developed by ShiroDev.
+Developed by ShiroDev. Commands are case Sensitive.
   /$$$$$$  /$$       /$$                     /$$$$$$$                      
  /$$__  $$| $$      |__/                    | $$__  $$                     
 | $$  \\__/| $$$$$$$  /$$  /$$$$$$   /$$$$$$ | $$  \\ $$  /$$$$$$  /$$    /$$
@@ -21,6 +21,7 @@ Developed by ShiroDev.
      [ [ /  \\/ _/
     _[ [ \\  /_/`.split("\n");
 
+console.log('in development');
 console.log("Run Terminal.clearLines() to clear terminal.\n Run showHeader to make a new header appear");
 console.log("Key for creating a line : ");
 console.log(`new Line("text", "css", mstime = 20, /* html tag type */ "pre" /*extra elements*/);`)
@@ -29,7 +30,6 @@ const Terminal = new terminal();
 let waiting = true;
 
 async function showHeader() {
-    console.log('in development');
 
     new Line("ShiroDev-Console ~ In development", `font-size: xx-large; color : red;`, 25);
 
@@ -43,6 +43,8 @@ async function showHeader() {
 
     await sleep(1000);
     waiting = false;
+    takeInput();
+
 }
 
 let typing = false;
@@ -56,23 +58,78 @@ async function takeInput(ev) {
         return;
     }
     if (!typing) {
-        console.log("hello?");
         new Line("Hello ~ ", "", 25, undefined);
         waiting = true;
         await sleep(225);
+        document.getElementById("Hello ~ ").innerText = document.getElementById("Hello ~ ").innerText += "▭";
         waiting = false;
         typing = true;
     }
 
-    if (ev.key == "shift") return;
 
-    if (ev.shiftKey) {
-        document.getElementById("Hello ~ ").textContent += ev.key.toUpperCase();
+    if (!ev) return;
+    if (!(ev.keyCode >= 48 && ev.keyCode <= 57 || ev.keyCode == 96) && !(ev.keyCode >= 65 && ev.keyCode <= 90) && !(ev.keyCode >= 97 && ev.keyCode <= 122) && ev.keyCode != 32 && ev.keyCode != "13" && ev.keyCode != 8) return;
+    document.getElementById("Hello ~ ").innerText = document.getElementById("Hello ~ ").innerText.replace("▭", "");
+    if (ev.key === "Backspace") {
+        if (document.getElementById("Hello ~ ").textContent == "Hello ~ ") return document.getElementById("Hello ~ ").textContent = document.getElementById("Hello ~ ").textContent += "▭";
+        document.getElementById("Hello ~ ").textContent = document.getElementById("Hello ~ ").textContent.slice(0, -1);
+        return document.getElementById("Hello ~ ").textContent = document.getElementById("Hello ~ ").textContent += "▭";
     }
 
+    if (ev.key == "Enter") {
+        typing = false;
+        let text = document.getElementById("Hello ~ ").textContent.replace("Hello ~ ", "");
+        document.getElementById("Hello ~ ").id = "";
+        return parseInput(text);
+    }
+
+    if (ev.shiftKey) {
+        document.getElementById("Hello ~ ").textContent += ev.key.toUpperCase() + "▭";
+        return;
+    }
+
+    document.getElementById("Hello ~ ").textContent += ev.key += "▭";
     return;
 }
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function parseInput(text) {
+    waiting = true;
+
+    switch (text) {
+        case "clear":
+            Terminal.clearLines();
+            waiting = false;
+            return takeInput();
+
+        case "help":
+            new Line("Current commands : ", undefined, 20);
+            new Line("clear : Clears the terminal", `color : green`, 20);
+            new Line("help : Sends info on current commands", `color : orange`, 20);
+            new Line("header : Shows the header", "color : red", 20);
+            new Line("home : Return to the home page", "color : white", 20);
+            await sleep(1200);
+
+            waiting = false;
+            return takeInput();
+
+        case "header":
+            showHeader();
+            waiting = false;
+            return;
+
+        case "home":
+            return window.location.href = "/";
+
+        default:
+            let timedText = `Command not found : ${text}`;
+            new Line("Command not found : " + text, "color : red", 20);
+
+            await sleep(timedText.length * 22);
+
+            waiting = false;
+            return takeInput();
+    }
 }
