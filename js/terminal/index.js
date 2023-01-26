64f7b4a2dc92262b8304a4f8aa5f5ad91e18f1f6
ShiroDevv <ayviej07@gmail.com>
2023-01-25 23:58:22 -0500

    Testing

--- js/terminal/index.js
+++ js/terminal/index.js
@@ -71,13 +71,9 @@ async function showHeader() {
 
 function mobileDevice() {
     console.log(navigator.userAgent);
-    if (navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
-        || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)) return true;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    }
     return false;
 }
 
